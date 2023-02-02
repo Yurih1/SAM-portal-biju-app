670ab8327f29f66264d292cc06f270ba480ce9bf
yurih1 <yurih2009@gmail.com>
2023-02-02 19:46:12 -0300

    feat: added structure

---
+++ get_itens/app.py
@@ -0,0 +1,12 @@
+import json
+
+# import requests
+
+# @execute_lambda
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+        }),
+    }
